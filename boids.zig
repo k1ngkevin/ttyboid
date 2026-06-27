2a9ea16c4e769cb39d3a865518fc713ec6d64eae
k1ngkevin <90162130+certifiedkaren@users.noreply.github.com>
2026-06-27 17:17:44 +0800

    added flock

--- boids.zig
+++ boids.zig
@@ -27,6 +27,13 @@ const Vector = struct {
             .y = min + random.float(f32) * (max - min),
         };
     }
+
+    pub fn randomPosition(random: std.Random, max_row: f32, max_col: f32) Vector {
+        return .{
+            .x = 1.0 + random.float(f32) * (max_row - 1.0),
+            .y = 1.0 + random.float(f32) * (max_col - 1.0),
+        };
+    }
 };
 
 const Boid = struct {
@@ -38,6 +45,13 @@ const Boid = struct {
         return .{
             .position = .{ .x = x, .y = y },
             .velocity = .{ .x = vx, .y = vy },
+        };
+    }
+
+    pub fn randomBoid(random: std.Random, max_row: f32, max_col: f32) Boid {
+        return .{
+            .position = Vector.randomPosition(random, max_row, max_col),
+            .velocity = Vector.randomVector(random, -1.0, 1.0),
         };
     }
 
@@ -73,7 +87,13 @@ pub fn main(init: std.process.Init) !void {
     var prng = std.Random.DefaultPrng.init(seed);
     const random = prng.random();
 
-    var my_boid = Boid.init(window_row / 2, window_col / 2, 0.1, 0.1);
+    var flock: [100]Boid = undefined;
+
+    for (&flock) |*boid| {
+        boid.* = Boid.randomBoid(random, window_size.row, window_size.col);
+    }
+
+    // var my_boid = Boid.init(window_row / 2, window_col / 2, 0, 0);
 
     const stdin_file = std.Io.File.stdin();
     var stdin = stdin_file.reader(init.io, &.{});
@@ -93,15 +113,19 @@ pub fn main(init: std.process.Init) !void {
     var key_vec = [_][]u8{&key_buf};
 
     while (true) {
-        my_boid.update(random);
-        const row_clamp = @min(@max(my_boid.position.x, 1.0), window_row);
-        const col_clamp = @min(@max(my_boid.position.y, 1.0), window_col);
-
-        const row: u16 = @intFromFloat(row_clamp);
-        const col: u16 = @intFromFloat(col_clamp);
-
+        // my_boid.update(random);
         try stdout_writer.print("\x1b[2J\x1b[H", .{}); // clear screen, move cursor home
-        try stdout_writer.print("\x1b[{};{}H↑", .{ row, col });
+
+        for (&flock) |*boid| {
+            boid.update(random);
+            const row_clamp = @min(@max(boid.position.x, 1.0), window_row);
+            const col_clamp = @min(@max(boid.position.y, 1.0), window_col);
+
+            const row: u16 = @intFromFloat(row_clamp);
+            const col: u16 = @intFromFloat(col_clamp);
+
+            try stdout_writer.print("\x1b[{};{}H#", .{ row, col });
+        }
         try stdout_writer.flush();
 
         const n = stdin_reader.readVec(&key_vec) catch |err| switch (err) {
